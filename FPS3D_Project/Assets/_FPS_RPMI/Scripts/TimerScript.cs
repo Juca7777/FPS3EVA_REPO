d7219995b2500eb148621cbd6748da03f7fda578
JuanCarlosHerediaJimenez <juancadev11@gmail.com>
2026-03-31 09:25:45 +0200

    Temporizador Script hecho

---
+++ FPS3D_Project/Assets/_FPS_RPMI/Scripts/TimerScript.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float tiempo = 30f;
+    public TextMeshProUGUI textoTiempo;
+
+    public GameObject explosionVFX;
+    public Transform puntoExplosion;
+
+    private bool haExplotado = false;
+    private float parpadeoTimer = 0f;
+
+    void Update()
+    {
+        if (haExplotado) return;
+
+        // Reducir tiempo
+        if (tiempo > 0)
+        {
+            tiempo -= Time.deltaTime;
+        }
+
+        // FORMATO 00:00
+        int minutos = Mathf.FloorToInt(tiempo / 60f);
+        int segundos = Mathf.FloorToInt(tiempo % 60f);
+        textoTiempo.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+
+        //PARPADEO PROGRESIVO EN LOS ÚLTIMOS 10s
+        if (tiempo <= 10f)
+        {
+            float velocidad = Mathf.Lerp(3f, 10f, 1f - (tiempo / 10f));
+            parpadeoTimer += Time.deltaTime * velocidad;
+
+            if (Mathf.FloorToInt(parpadeoTimer) % 2 == 0)
+                textoTiempo.color = Color.red;
+            else
+                textoTiempo.color = Color.white;
+        }
+        else
+        {
+            textoTiempo.color = Color.white;
+        }
+
+        //EXPLOSIÓN
+        if (tiempo <= 0 && !haExplotado)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        haExplotado = true;
+
+        Instantiate(explosionVFX, puntoExplosion.position, Quaternion.identity);
+
+        textoTiempo.enabled = false;
+
+        Invoke(nameof(PerderNivel), 2f);
+    }
+
+    void PerderNivel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
